061f36d4f2a9babf3aedd346323a421385e3a595
LAjoyan <ajoyan.lilit@gmail.com>
2026-04-21 00:38:53 +0200

    feat: integrate logging middleware into FastAPI app for request tracking

--- customer_suport/src/customer_suport/backend/api.py
+++ customer_suport/src/customer_suport/backend/api.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from customer_suport.backend.agents import support_agent
+from customer_suport.src.customer_suport.backend.middlewares import logging_middleware
 
 app = FastAPI()
+logging_middleware(app=app)
+
 
 # get for simplicity -> in real cases use post
-@app.get('/customer_support')
-async def customer_support_faq(question:str) -> str:
+@app.get("/customer_support")
+async def customer_support_faq(question: str) -> str:
     result = await support_agent.run(question)
     return result.output
